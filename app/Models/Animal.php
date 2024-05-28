ed755e0015a4b4b97ab147cadfeda49fca8abd28
jimmy.hong <pklaz0078@gmail.com>
2024-05-28 20:52:51 +0800

    create animals resource

---
+++ app/Models/Animal.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Animal extends Model
+{
+    use HasFactory;
+    /**
+     * 可以被批量賦值的屬性。
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'type_id',
+        'name',
+        'birthday',
+        'area',
+        'fix',
+        'description',
+        'personality',
+    ];
+}
